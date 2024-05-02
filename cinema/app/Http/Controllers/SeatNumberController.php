4640ebabb1a9555535cdd4bde8b51c428e16ec89
aleksandr-suchugov <alex.coup@yandex.ru>
2024-05-02 11:22:07 +0300

    controllers

---
+++ cinema/app/Http/Controllers/SeatNumberController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SeatNumber;
+use Illuminate\Http\Request;
+
+class SeatNumberController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SeatNumber $seatNumber)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(SeatNumber $seatNumber)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, SeatNumber $seatNumber)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SeatNumber $seatNumber)
+    {
+        //
+    }
+}
